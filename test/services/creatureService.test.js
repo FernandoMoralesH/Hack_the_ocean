dbdfae7d78789572d868619cde608de97fb868dd
David Patiño <david.andrespr5@gmail.com>
2022-05-14 15:19:05 -0500

    Agregando pruebas de unidad del metodo getCreaturebyName

--- test/services/creatureService.test.js
+++ test/services/creatureService.test.js
@@ -1,9 +1,14 @@
-const StudentService = require("./../../lib/services/creatureService");
+const CreatureService = require("./../../lib/services/creatureService");
 const Reader = require("./../../lib/utils/Reader");
 describe("Class CreatureService",()=>{
     test("Metodo getCreatures",()=>{
         const creatures = Reader.readJsonFile("creatures.json");
-        const getCreatures = StudentService.getCreatures(creatures);
+        const getCreatures = CreatureService.getCreatures(creatures);
         expect(getCreatures).not.toBeUndefined();
+    });
+    test("2. Metodo CreaturebyName",()=>{
+        const creatures = Reader.readJsonFile("creatures.json");
+        const CreaturebyName= CreatureService.getCreturebyName(creatures,"Totoaba");
+        expect(CreaturebyName).not.toBeUndefined();
     });
 });
